335c688686d33f00b20131b71f32d346a0e8f867
aaron levin <aaron.levin@soundcloud.com>
2016-11-23 16:04:09 +0100

    Optionally coalesce cartesian product in exact NN

--- src/main/scala/com/soundcloud/lsh/QueryNearestNeighbours.scala
+++ src/main/scala/com/soundcloud/lsh/QueryNearestNeighbours.scala
@@ -8,17 +8,21 @@ import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, IndexedRow, 
  * parameters may be passed such that neighbours are just computed for a random fraction of
  * the data set.
  *
- * @param distance        a function defining a metric over a vector space
- * @param threshold       pairs that are >= to the distance are discarded
- * @param queryFraction   compute for the random fraction of queries
- * @param catalogFraction compute for the random fraction of the catalog
+ * @param distance            a function defining a metric over a vector space
+ * @param threshold           pairs that are >= to the distance are discarded
+ * @param queryFraction       compute for the random fraction of queries
+ * @param catalogFraction     compute for the random fraction of the catalog
+ * @param cartesianPartitions an optional number of partitions to pass to the 
+ *                            cartesian product for coalescing (the product can
+ *                            produce a large number of tasks).
  *
  */
 class QueryNearestNeighbours(
                               distance: VectorDistance,
                               threshold: Double,
                               queryFraction: Double,
-                              catalogFraction: Double
+                              catalogFraction: Double,
+                              cartesianPartitions: Option[Int] = None
                             ) extends QueryJoiner with Serializable {
 
   def join(queryMatrix: IndexedRowMatrix, catalogMatrix: IndexedRowMatrix): CoordinateMatrix = {
@@ -26,6 +30,8 @@ class QueryNearestNeighbours(
     val sampledCatalog = catalogMatrix.rows.sample(false, catalogFraction)
 
     val joined = sampledQueries.cartesian(sampledCatalog)
+
+    cartesianPartitions.foreach { p => joined.coalesce(p) }
 
     val neighbours = joined.map { case ((query: IndexedRow), (catalogEntry: IndexedRow)) =>
       new MatrixEntry(query.index, catalogEntry.index, distance(query.vector, catalogEntry.vector))
