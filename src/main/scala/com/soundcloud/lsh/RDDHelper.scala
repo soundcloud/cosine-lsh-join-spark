b1e821830521fae56c3d4b029e9732799223b75f
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2018-05-04 00:33:18 +0200

    More Georges, simplify helper.

--- src/main/scala/com/soundcloud/lsh/RDDHelper.scala
+++ src/main/scala/com/soundcloud/lsh/RDDHelper.scala
@@ -6,16 +6,12 @@ import org.apache.spark.rdd.RDD
 import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
-final case class HelperRDDFunctions[A](self: RDD[A]) extends AnyVal {
-  /**
-   * Applies a partial function to an RDD. This is useful for chaining map or
-   * filter functions onto an RDD as you might with a core Scala collection.
-   */
-  def andThen[T](fn: (RDD[A]) => RDD[T]): RDD[T] = fn(self)
-
-}
-
 object SparkImplicits {
-  implicit def rddToHelperRDDFunctions[A](rdd: RDD[A]): HelperRDDFunctions[A] =
-    HelperRDDFunctions(rdd)
+  implicit class HelperRDDFunctions[A](val self: RDD[A]) extends AnyVal {
+    /**
+     * Applies a partial function to an RDD. This is useful for chaining map or
+     * filter functions onto an RDD as you might with a core Scala collection.
+     */
+    def andThen[T](fn: (RDD[A]) => RDD[T]): RDD[T] = fn(self)
+  }
 }
